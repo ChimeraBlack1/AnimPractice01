9bb4412c9c92525f4819b225fb6ae5ebc1feea02
chimerablack1 <chimerablaque@hotmail.com>
2019-03-09 13:14:53 -0500

    Added basic network components and removed player prefab from the scene.  The game will now instantiate a player when the server is hosted, and when a client connects.  control has been localized

---
+++ Assets/Characters/Warrior/SetupLocalPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SetupLocalPlayer : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        if (isLocalPlayer)
+        {
+            GetComponent<knightControls>().enabled = true;
+        }
+        else
+        {
+            GetComponent<knightControls>().enabled = false;
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
